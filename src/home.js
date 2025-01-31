c682d5c61f34a54c378fdc295fdc155651f2eab0
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-01-31 17:07:14 +0000

    fixing template & removing restaurant page

--- src/home.js
+++ src/home.js
@@ -3,12 +3,12 @@ export const  homePage = () => {
     const content = document.querySelector(".content");
     content.textContent = "";
     const h1 = document.createElement("h1");
-    h1.id  = "restaurant-title";
-    h1.textContent = "Falcon's Fabolous Taverna"
+    h1.id  = "title";
+    h1.textContent = "Some text"
     
     const p  = document.createElement("p");
-    p.id= "restaurant-description";
-    p.textContent = "Welcome to Falcon's Fabolous Taverna, opened in the 80s in Holborn, a staple of Greek quisine in London";
+    p.id= "description";
+    p.textContent = "Some description";
     content.append(h1, p);
   
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import "./styles.css";
-import {homePage} from "./home.js";
-import { menu} from "./menu.js";
-import {about } from "./about.js";
-import {contact } from "./contact.js";
-const homeButton = document.querySelector("#home")
-const menuButton = document.querySelector("#menu");
-const aboutButton = document.querySelector("#about");
-const contactButton = document.querySelector("#contact");
-
-homeButton.addEventListener("click", () => homePage());
-menuButton.addEventListener("click", ()=> menu());
-aboutButton.addEventListener("click", ()=> about());
-contactButton.addEventListener("click", ()=> contact() )
-homePage();
 
 
 console.log("Hello world");
